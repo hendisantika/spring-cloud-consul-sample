649e27fd3b78a2569bde804ffc02d2b65777288e
hendisantika <hendisantika@yahoo.co.id>
2025-01-25 11:34:26 +0700

    Add requestLoggingFilter in Main Class🫸🌀✏️📗🐧🐳⬆

--- product-service/src/main/java/id/my/hendisantika/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/id/my/hendisantika/productservice/ProductServiceApplication.java
@@ -2,6 +2,8 @@ package id.my.hendisantika.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class ProductServiceApplication {
@@ -10,4 +12,13 @@ public class ProductServiceApplication {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setIncludeHeaders(true);
+		loggingFilter.setMaxPayloadLength(1000);
+		loggingFilter.setAfterMessagePrefix("REQ:");
+		return loggingFilter;
+	}
 }
