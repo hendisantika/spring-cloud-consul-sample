9d4ad78ff6786ef31b2c22cb6499bbad3d6b3d5f
hendisantika <hendisantika@yahoo.co.id>
2025-01-25 11:31:25 +0700

    Add ProductRepository🫸🌀✏️📗🐧🐳⬆

---
+++ product-service/src/main/java/id/my/hendisantika/productservice/repository/ProductRepository.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.productservice.repository;
+
+import id.my.hendisantika.productservice.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-cloud-consul-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 25/01/25
+ * Time: 11.31
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProductRepository {
+    private final List<Product> products = new ArrayList<>();
+}
