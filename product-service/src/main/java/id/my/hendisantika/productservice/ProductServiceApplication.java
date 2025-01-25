950f450b9810fccdb3f533a11a20d4af21af682d
hendisantika <hendisantika@yahoo.co.id>
2025-01-25 11:35:06 +0700

    Add productRepository in Main Class🫸🌀✏️📗🐧🐳⬆

--- product-service/src/main/java/id/my/hendisantika/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/id/my/hendisantika/productservice/ProductServiceApplication.java
@@ -1,5 +1,7 @@
 package id.my.hendisantika.productservice;
 
+import id.my.hendisantika.productservice.model.Product;
+import id.my.hendisantika.productservice.repository.ProductRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -20,5 +22,21 @@ public class ProductServiceApplication {
 		loggingFilter.setMaxPayloadLength(1000);
 		loggingFilter.setAfterMessagePrefix("REQ:");
 		return loggingFilter;
+	}
+
+	@Bean
+	public ProductRepository productRepository() {
+		ProductRepository repository = new ProductRepository();
+		repository.add(new Product("Test1", 1000));
+		repository.add(new Product("Test2", 1500));
+		repository.add(new Product("Test3", 2000));
+		repository.add(new Product("Test4", 3000));
+		repository.add(new Product("Test5", 1300));
+		repository.add(new Product("Test6", 2700));
+		repository.add(new Product("Test7", 3500));
+		repository.add(new Product("Test8", 1250));
+		repository.add(new Product("Test9", 2450));
+		repository.add(new Product("Test10", 800));
+		return repository;
 	}
 }
--- product-service/src/main/java/id/my/hendisantika/productservice/model/Product.java
+++ product-service/src/main/java/id/my/hendisantika/productservice/model/Product.java
@@ -22,4 +22,9 @@ public class Product {
     private Long id;
     private String name;
     private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
 }
