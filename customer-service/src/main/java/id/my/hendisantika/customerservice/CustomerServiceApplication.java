9f098c353103c0a6d17daf8d02f8a1bb84e026b7
hendisantika <hendisantika@yahoo.co.id>
2025-01-25 11:04:53 +0700

    Add requestLoggingFilter🫸🌀✏️📗🐧🐳⬆

--- customer-service/src/main/java/id/my/hendisantika/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/id/my/hendisantika/customerservice/CustomerServiceApplication.java
@@ -2,12 +2,25 @@ package id.my.hendisantika.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
 
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(true);
+        loggingFilter.setMaxPayloadLength(1000);
+        loggingFilter.setAfterMessagePrefix("REQ:");
+        return loggingFilter;
+    }
 }
