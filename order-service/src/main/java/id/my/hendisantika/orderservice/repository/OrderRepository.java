ad4a3a998edaac09b104b28b5cbd9ac910499af9
hendisantika <hendisantika@yahoo.co.id>
2025-01-25 11:18:19 +0700

    Add OrderRepository🫸🌀✏️📗🐧🐳⬆

---
+++ order-service/src/main/java/id/my/hendisantika/orderservice/repository/OrderRepository.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.orderservice.repository;
+
+import id.my.hendisantika.orderservice.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-cloud-consul-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 25/01/25
+ * Time: 11.18
+ * To change this template use File | Settings | File Templates.
+ */
+public class OrderRepository {
+    private final List<Order> orders = new ArrayList<>();
+}
