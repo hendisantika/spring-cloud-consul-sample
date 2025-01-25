8ca1ce8801c41787b0377301c1e348de4737f357
hendisantika <hendisantika@yahoo.co.id>
2025-01-25 10:53:03 +0700

    Add accountRepository in Main Class🫸🌀✏️📗🐧🐳⬆

--- account-service/src/main/java/id/my/hendisantika/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/id/my/hendisantika/accountservice/AccountServiceApplication.java
@@ -1,5 +1,7 @@
 package id.my.hendisantika.accountservice;
 
+import id.my.hendisantika.accountservice.model.Account;
+import id.my.hendisantika.accountservice.repository.AccountRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -20,5 +22,20 @@ public class AccountServiceApplication {
 		loggingFilter.setMaxPayloadLength(1000);
 		loggingFilter.setAfterMessagePrefix("REQ:");
 		return loggingFilter;
+	}
+
+	@Bean
+	public AccountRepository accountRepository() {
+		AccountRepository repository = new AccountRepository();
+		repository.add(new Account("1234567890", 50000, 1L));
+		repository.add(new Account("1234567891", 50000, 1L));
+		repository.add(new Account("1234567892", 50000, 1L));
+		repository.add(new Account("1234567893", 50000, 2L));
+		repository.add(new Account("1234567894", 50000, 2L));
+		repository.add(new Account("1234567895", 50000, 2L));
+		repository.add(new Account("1234567896", 50000, 3L));
+		repository.add(new Account("1234567897", 50000, 3L));
+		repository.add(new Account("1234567898", 50000, 3L));
+		return repository;
 	}
 }
