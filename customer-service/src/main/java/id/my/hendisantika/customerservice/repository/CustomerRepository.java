c07a743fa2f9c8044df48237a5ca317494d5a272
hendisantika <hendisantika@yahoo.co.id>
2025-01-25 10:59:48 +0700

    Add CustomerRepository🫸🌀✏️📗🐧🐳⬆

---
+++ customer-service/src/main/java/id/my/hendisantika/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.customerservice.repository;
+
+import id.my.hendisantika.customerservice.model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-cloud-consul-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 25/01/25
+ * Time: 10.59
+ * To change this template use File | Settings | File Templates.
+ */
+public class CustomerRepository {
+    private final List<Customer> customers = new ArrayList<>();
+}
