32e5be235d7aaf8059a22278de84e75030cabc37
hendisantika <hendisantika@yahoo.co.id>
2025-01-25 11:33:43 +0700

    Add find in ProductController🫸🌀✏️📗🐧🐳⬆

--- product-service/src/main/java/id/my/hendisantika/productservice/controller/ProductController.java
+++ product-service/src/main/java/id/my/hendisantika/productservice/controller/ProductController.java
@@ -1,5 +1,6 @@
 package id.my.hendisantika.productservice.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import id.my.hendisantika.productservice.model.Product;
 import id.my.hendisantika.productservice.repository.ProductRepository;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -44,5 +48,12 @@ public class ProductController {
     @GetMapping("/{id}")
     public Product findById(@PathVariable("id") Long id) {
         return repository.findById(id);
+    }
+
+    @PostMapping("/ids")
+    public List<Product> find(@RequestBody List<Long> ids) throws JsonProcessingException {
+        List<Product> products = repository.find(ids);
+        LOGGER.info("Products found: {}", mapper.writeValueAsString(Collections.singletonMap("count", products.size())));
+        return products;
     }
 }
