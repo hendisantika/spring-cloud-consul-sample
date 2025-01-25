0100a1d13c0c85f2385eecdf9abd8ba0c46e6647
hendisantika <hendisantika@yahoo.co.id>
2025-01-25 11:33:02 +0700

    Add ProductController🫸🌀✏️📗🐧🐳⬆

---
+++ product-service/src/main/java/id/my/hendisantika/productservice/controller/ProductController.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.productservice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-cloud-consul-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 25/01/25
+ * Time: 11.32
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductRepository productRepository;
+    private final ObjectMapper mapper = new ObjectMapper();
+}
