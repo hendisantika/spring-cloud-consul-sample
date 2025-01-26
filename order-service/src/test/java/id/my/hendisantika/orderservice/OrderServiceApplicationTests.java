41ecc6f7a45dd7e98f1b7e4de676ec2ad6461d3e
hendisantika <hendisantika@yahoo.co.id>
2025-01-26 07:11:38 +0700

    Add OrderServiceApplicationTests🫸🌀✏️📗🐧🐳⬆

--- order-service/src/test/java/id/my/hendisantika/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/id/my/hendisantika/orderservice/OrderServiceApplicationTests.java
@@ -1,13 +1,30 @@
 package id.my.hendisantika.orderservice;
 
-import org.junit.jupiter.api.Test;
+import io.specto.hoverfly.junit5.HoverflyExtension;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.testcontainers.consul.ConsulContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@ExtendWith(HoverflyExtension.class)
 class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Container
+	static ConsulContainer consulContainer = new ConsulContainer("consul:1.14")
+			.withConsulCommand("kv put config/order-service test=abc");
+	@Autowired
+	TestRestTemplate template;
+
+	@BeforeAll
+	static void init() {
+		System.setProperty("spring.cloud.consul.port", consulContainer.getFirstMappedPort().toString());
+		System.setProperty("spring.config.import", "optional:consul:localhost:" + consulContainer.getFirstMappedPort());
 	}
 
 }
