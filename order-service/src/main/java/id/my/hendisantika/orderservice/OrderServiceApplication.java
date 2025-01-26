a2aa85cb965068cde894f6eb5024c40dab3d11bd
hendisantika <hendisantika@yahoo.co.id>
2025-01-26 10:21:46 +0700

    fix: Bean🫸🌀✏️📗🐧🐳⬆

--- order-service/src/main/java/id/my/hendisantika/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/id/my/hendisantika/orderservice/OrderServiceApplication.java
@@ -1,9 +1,12 @@
 package id.my.hendisantika.orderservice;
 
+import id.my.hendisantika.orderservice.repository.OrderRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,4 +17,19 @@ public class OrderServiceApplication {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
+	@Bean
+	public OrderRepository repository() {
+		return new OrderRepository();
+	}
+
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setIncludeHeaders(true);
+		loggingFilter.setMaxPayloadLength(1000);
+		loggingFilter.setAfterMessagePrefix("REQ:");
+		return loggingFilter;
+	}
 }
+
