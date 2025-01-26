9756c0a4f8c79218d3fd0eb434080ff7e943a4f7
hendisantika <hendisantika@yahoo.co.id>
2025-01-26 08:32:05 +0700

    feat: Add Account Model🫸🌀✏️📗🐧🐳⬆

--- gateway-service/src/test/java/id/my/hendisantika/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/id/my/hendisantika/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,7 +1,6 @@
 package id.my.hendisantika.gatewayservice;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +9,6 @@ import org.testcontainers.consul.ConsulContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
