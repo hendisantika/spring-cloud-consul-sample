a54f573533fb353590807a1a1fb77fb679f89809
hendisantika <hendisantika@yahoo.co.id>
2025-01-26 07:08:56 +0700

    Add GatewayServiceApplicationTests🫸🌀✏️📗🐧🐳⬆

--- gateway-service/src/test/java/id/my/hendisantika/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/id/my/hendisantika/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,13 +1,27 @@
 package id.my.hendisantika.gatewayservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.testcontainers.consul.ConsulContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class GatewayServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Container
+	static ConsulContainer consulContainer = new ConsulContainer("consul:1.14")
+			.withConsulCommand("kv put config/customer-service test=abc");
+	@Autowired
+	TestRestTemplate template;
+
+	@BeforeAll
+	static void init() {
+		System.setProperty("spring.cloud.consul.port", consulContainer.getFirstMappedPort().toString());
+		System.setProperty("spring.config.import", "optional:consul:localhost:" + consulContainer.getFirstMappedPort());
 	}
 
 }
