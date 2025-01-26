2f019eff74e5c6fc42c1f6f9e36ef32be3085257
hendisantika <hendisantika@yahoo.co.id>
2025-01-26 07:12:09 +0700

    Add testOrder in OrderServiceApplicationTests🫸🌀✏️📗🐧🐳⬆

--- order-service/src/test/java/id/my/hendisantika/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/id/my/hendisantika/orderservice/OrderServiceApplicationTests.java
@@ -1,7 +1,9 @@
 package id.my.hendisantika.orderservice;
 
+import io.specto.hoverfly.junit.core.Hoverfly;
 import io.specto.hoverfly.junit5.HoverflyExtension;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +11,10 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.testcontainers.consul.ConsulContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static io.specto.hoverfly.junit.core.SimulationSource.dsl;
+import static io.specto.hoverfly.junit.dsl.HoverflyDsl.service;
+import static io.specto.hoverfly.junit.dsl.ResponseCreators.success;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
@@ -27,4 +33,36 @@ class OrderServiceApplicationTests {
 		System.setProperty("spring.config.import", "optional:consul:localhost:" + consulContainer.getFirstMappedPort());
 	}
 
+	@Test
+	public void testOrder(Hoverfly hoverfly) throws InterruptedException {
+		hoverfly.simulate(
+				dsl(service("http://customer-service")
+						.get("/withAccounts/1")
+						.willReturn(success().body(
+								"""
+										{
+										  "id":1,
+										  "type":"REGULAR",
+										  "accounts":[{
+										    "id":1,
+										    "balance":10000
+										  }]
+										}
+										"""
+						).header("Content-Type", "application/json"))),
+				dsl(service("http://product-service")
+						.post("/ids").body("[1]")
+						.willReturn(success().body(
+								"""
+										[{
+										  "id":1,
+										  "price":1000,
+										  "name":"Test1"
+										}]
+										"""
+						).header("Content-Type", "application/json")))
+		);
+
+		sendAndAcceptOrder();
+	}
 }
