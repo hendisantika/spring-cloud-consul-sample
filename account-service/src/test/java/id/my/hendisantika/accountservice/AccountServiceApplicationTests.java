e76628e4fe7da821b23930f25674f3f9f4e47ad1
hendisantika <hendisantika@yahoo.co.id>
2025-01-26 08:08:45 +0700

    fix: Integration Test🫸🌀✏️📗🐧🐳⬆

--- account-service/src/test/java/id/my/hendisantika/accountservice/AccountServiceApplicationTests.java
+++ account-service/src/test/java/id/my/hendisantika/accountservice/AccountServiceApplicationTests.java
@@ -1,14 +1,16 @@
 package id.my.hendisantika.accountservice;
 
 import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Ignore
+@Disabled("Disabled until Account Service is up!")
 class AccountServiceApplicationTests {
 
 	@Test
+	@Ignore
 	void contextLoads() {
 	}
 
